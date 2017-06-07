23b2a9173a8bd9c24f1ce9550e97a5511d902329
ecoles-cgi <edgar.coles@cgi.com>
2017-06-07 09:50:50 -0600

    missing import

--- src/main/java/io/cfapps/serviceregistry/ServiceRegistryApplication.java
+++ src/main/java/io/cfapps/serviceregistry/ServiceRegistryApplication.java
@@ -2,6 +2,7 @@ package io.cfapps.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
